6680155e1230797aed4587a6242d44e97770de33
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-01 19:16:54 +0300

    рабочая программа

--- Zadacha4/Program.cs
+++ Zadacha4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine($"{a}, {b}, {c} -> {a}");
+    }
+    else
+    {
+        Console.WriteLine($"{a}, {b}, {c} -> {c}");
+    }
+}
+
+else if (b > c)
+{
+    Console.WriteLine($"{a}, {b}, {c} -> {b}");
+}
+else
+{
+    Console.WriteLine($"{a}, {b}, {c} -> {c}");
+}
