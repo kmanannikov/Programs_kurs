ac2572732c9c3f7d7d020451dc79f18788ccb8cb
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-01 19:37:55 +0300

    готовая задача

--- Zafacha6/Program.cs
+++ Zafacha6/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 2 == 1)
+if (a % 2 == 0)
 {
-    Console.WriteLine($"{a} -> нет");
+    Console.WriteLine($"{a} -> да");
 }
 else
 {
-    Console.WriteLine($"{a} -> да");
+    Console.WriteLine($"{a} -> нет");
 }
