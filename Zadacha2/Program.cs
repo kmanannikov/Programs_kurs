cdd3add7a38440b31a0c4b8a504ce715e82082ff
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-01 18:29:49 +0300

    рабочая программа

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число 1: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine(numA);
+}
+else 
+{
+    Console.WriteLine(numB);
+}
