4f4f571832d5c653fd1a60fffd568cfad595f4cb
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-01 19:35:35 +0300

    работает

--- Zafacha6/Program.cs
+++ Zafacha6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+    Console.WriteLine($"{a} -> нет");
+}
+else
+{
+    Console.WriteLine($"{a} -> да");
+}
