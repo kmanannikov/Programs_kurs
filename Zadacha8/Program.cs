b3bc81543402309ca92f54a1da71038e22c284cd
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-01 21:17:15 +0300

    готовая задача

--- Zadacha8/Program.cs
+++ Zadacha8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{a} -> ");
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел");
+}
