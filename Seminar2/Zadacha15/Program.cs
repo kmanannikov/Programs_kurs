7658b27f6efdda296f418d20d6567e0c2d46fcad
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-03 17:36:54 +0300

    готовая задача

---
+++ Seminar2/Zadacha15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели числом: ");
+int num = int.Parse(Console.ReadLine());
+
+bool result = (num < 1) || (num > 7);
+
+if (result)
+{
+    Console.WriteLine("Введено не верное число");
+}
+
+else
+{
+    if (num < 6)
+    {
+        Console.WriteLine($"{num} -> Нет");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> Да");
+    }
+
+}
