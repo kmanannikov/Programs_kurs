0e0df713f3e69e4d547df0d19a62beb3c98d94bf
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-03 17:19:58 +0300

    готовая задача

--- Seminar2/Zadacha13/Program.cs
+++ Seminar2/Zadacha13/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int a = num;
+
+bool result = (num < 100);
+
+if (result)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    while (a > 999)
+    {
+       a = a / 10;
+    }
+    Console.WriteLine($"{num} -> {a % 10}");
+}
