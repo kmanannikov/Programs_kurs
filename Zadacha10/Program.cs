5a405802bb61fbea861650862d7e113fced76f5f
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-03 16:29:36 +0300

    готовая задача

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+//int b = ((num / 10) % 10);
+
+bool result = (num < 100) || (num > 999);
+
+if (result)
+{
+    Console.WriteLine("Введено не верное число");
+}
+
+else
+{
+    Console.WriteLine($"{num} -> {((num / 10) % 10)}");
+}
