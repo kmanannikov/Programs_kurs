7739bedaeaf10d4abb1bfbb44592a13dec706553
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-06 18:48:52 +0300

    готовая задача

--- Seminar3/Zadacha23/Program.cs
+++ Seminar3/Zadacha23/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
