130a6466e1f7076852167bb9e9da907486da368e
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-06 17:00:30 +0300

    Готовая задача

--- Seminar3/Zadacha21/Program.cs
+++ Seminar3/Zadacha21/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int aX, aY, aZ;
+int bX, bY, bZ;
+double resultDoub;
+
+Console.WriteLine("Поочередно введите координаты точек: ");
+Console.Write("aX: ");
+aX = Convert.ToInt32(Console.ReadLine());
+Console.Write("aY: ");
+aY = Convert.ToInt32(Console.ReadLine());
+Console.Write("aZ: ");
+aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("bX: ");
+bX = Convert.ToInt32(Console.ReadLine());
+Console.Write("bY: ");
+bY = Convert.ToInt32(Console.ReadLine());
+Console.Write("bZ: ");
+bZ = Convert.ToInt32(Console.ReadLine());
+
+resultDoub = Math.Pow(bX - aX, 2) + Math.Pow(bY - aY, 2) + Math.Pow(bZ - aZ, 2);
+resultDoub = Math.Sqrt(resultDoub);
+resultDoub = Math.Round(resultDoub, 2);
+Console.WriteLine($"A({aX},{aY},{aZ}); B({bX},{bY},{bZ}), -> {resultDoub}");
