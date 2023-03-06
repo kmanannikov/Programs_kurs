c6f342aedf4efc73565e4e6d0ae6be2b84142452
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-06 19:00:27 +0300

    готовая задача

--- Seminar3/Zadacha19/Program.cs
+++ Seminar3/Zadacha19/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string numStr = Console.ReadLine();
+
+int Num1 = 0;
+int Num2 = numStr.Length - 1;
+
+if (Num2 == 4)
+{
+    while (Num1 < Num2 && Char.IsDigit(numStr[Num1]) && Char.IsDigit(numStr[Num2]))
+    {
+        if (numStr[Num1] == numStr[Num2])
+        {
+            Num1++;
+            Num2 = Num2 - 1;
+        }
+        else
+        {
+            Console.WriteLine($"{numStr} -> нет");
+            Num1 = 5;
+        }
+    }
+    if (Num1 == 2) Console.WriteLine($"{numStr} -> да");
+}
+
+else Console.WriteLine("Введено не верное значение");
