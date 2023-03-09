efa07b5e53eadc4929bdc94fcda76ec6a3a62183
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-09 11:21:56 +0300

    готовая задача

---
+++ Seminar4/Zadacha25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+
+int NumA, NumB;
+
+NumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+NumB = Convert.ToInt32(Console.ReadLine());
+
+int result = NumA;
+
+for (int i = 2; i <= NumB; i++)
+{
+    result *= NumA;
+}
+
+Console.WriteLine($"{NumA}, {NumB} -> {result}");
