736f97b325f1f7b26f3c4ca357ea6c9eb37bfb70
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-09 11:31:55 +0300

    готовая задача

---
+++ Seminar4/Zadacha27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (Num != 0)
+{
+    result += Num % 10;
+    Num /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе -> {result}");
