9af81c7eb7a5c0557cb8c30683319307cf6dba5a
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-14 10:33:22 +0300

    готовая задача

---
+++ Seminar6/Zadacha43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}, {y})");
