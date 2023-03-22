ffaf9c0dcfb011fb8fb73a66ec13759237cfffa3
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-22 15:21:33 +0300

    готовая задача

--- Seminar8/Zadacha58/Program.cs
+++ Seminar8/Zadacha58/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("Введите размер матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixProduct = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixProduct[i, j] = matrixProduct[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Матрица - А");
+Console.WriteLine();
+PrintArray(matrixA);
+
+Console.WriteLine();
+Console.WriteLine("Матрица - В");
+Console.WriteLine();
+PrintArray(matrixB);
+
+Console.WriteLine();
+Console.WriteLine("Произведение двух матриц");
+Console.WriteLine();
+PrintArray(matrixProduct);
+Console.WriteLine();
