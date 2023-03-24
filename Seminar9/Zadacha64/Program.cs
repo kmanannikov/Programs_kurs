35ed613afc4196acc34e8e803dbd82d01f56df99
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-24 16:42:40 +0300

    готовая задача

---
+++ Seminar9/Zadacha64/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int n = InputInt("Введите значение N: ");
+int m = 1;
+
+if (n < 1)
+{
+    Console.WriteLine("Введено неверное значение");
+}
+
+Console.Write($"N = {n} -> ");
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
