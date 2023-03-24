d9ea9edd7e724e8bd06d72e5b33bf05e2876ed74
Kirill Manannikov <kmanannikov@yandex.ru>
2023-03-24 17:07:44 +0300

    готовая задача

---
+++ Seminar9/Zadacha68/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Необходимо ввести два положительных числа");
+int m = InputInt("Введите значение m: ");
+int n = InputInt("Введите значение n: ");
+
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
